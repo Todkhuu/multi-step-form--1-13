75deea8cd097b5de4c781404f2280c30f8cf8342
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-01-13 12:56:07 +0800

    first name

--- src/components/FirstName.jsx
+++ src/components/FirstName.jsx
@@ -1,6 +1,6 @@
+import { Input } from "./Input";
 
 export const FirstName = () => {
-  const names = ["First name", "Last name", "Username"];
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div className="w-[480px] h-[655px] rounded-lg bg-white p-8 flex flex-col justify-between">
@@ -15,18 +15,23 @@ export const FirstName = () => {
             </p>
           </div>
           <div className="flex flex-col gap-3 mt-[28px]">
-            {names.map((name) => (
-              <div className="flex flex-col gap-2">
-                <p className="text-[#334155] text-[14px] font-semibold ">
-                  {name}
-                </p>
-                <input
-                  className="border-[#334155] border-[1px] rounded-lg outline-none w-[416px] h-[40px] text-base p-3 active:border-[#0CA5e9]"
-                  type="text"
-                  placeholder="Placeholder"
-                />
-              </div>
-            ))}
+            <Input
+              label={"First name"}
+              placeholder={"Your first name"}
+              error={"First name cannot contain special characters or numbers."}
+            />
+            <Input
+              label={"Last name"}
+              placeholder={"Your last name"}
+              error={"Last name cannot contain special characters or numbers."}
+            />
+            <Input
+              label={"Username"}
+              placeholder={"Your username"}
+              error={
+                "This username is already taken. Please choose another one."
+              }
+            />
           </div>
         </div>
         <button className="w-[416] h-[44px] bg-[#121316] rounded-[6px] text-white ">
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,17 @@
-export const Input = ({ label, placeholder }) => {
+export const Input = ({ label, placeholder, error }) => {
+  //   const error = null;
   return (
     <div className="flex flex-col gap-2">
-      <p className="text-[#334155] text-[14px] font-semibold ">{label}</p>
+      <p className="text-[#334155] text-[14px] font-semibold ">
+        {label}
+        <span className="text-[#e14942]">*</span>
+      </p>
       <input
         className="border-[#334155] border-[1px] rounded-lg outline-none w-[416px] h-[40px] text-base p-3 active:border-[#0CA5e9]"
         type="text"
         placeholder={placeholder}
       />
+      <p className="text-[14px] text-[#e14942] ">{error}</p>
     </div>
   );
 };
